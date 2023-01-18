458a954e39e49e9aa28acfccddb20d64b84d6c59
hongyeongjune <hongyeongjune@naver.com>
2023-01-18 23:33:04 +0900

    refactor java repository -> kotlin repository

--- src/main/java/com/group/libraryapp/domain/user/JavaUserRepository.java
+++ src/main/java/com/group/libraryapp/domain/user/JavaUserRepository.java
@@ -4,7 +4,7 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface JavaUserRepository extends JpaRepository<User, Long> {
 
   Optional<User> findByName(String name);
 
