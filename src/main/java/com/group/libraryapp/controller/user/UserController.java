514e8a8a6e9e145b8c5afda24dbb275a5f9df9bb
hongyeongjune <hongyeongjune@naver.com>
2023-01-25 00:42:47 +0900

    refactor java dto -> kotlin dto

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -1,12 +1,20 @@
 package com.group.libraryapp.controller.user;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.group.libraryapp.dto.user.request.JavaUserUpdateRequest;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
 import com.group.libraryapp.service.user.UserService;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 public class UserController {
--- src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
+++ src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
@@ -24,7 +24,7 @@ class UserService(
     @Transactional
     fun getUsers(): List<UserResponse> {
         return userRepository.findAll()
-            .map(::UserResponse)
+            .map(UserResponse.Companion::of)
     }
 
     @Transactional
