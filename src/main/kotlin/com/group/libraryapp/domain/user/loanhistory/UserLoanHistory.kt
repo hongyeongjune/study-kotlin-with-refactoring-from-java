0a211a89eeb1aa992978bc5fc0586c1e0e8afe62
hongyeongjune <hongyeongjune@naver.com>
2023-01-28 12:52:30 +0900

    refactor N+1 problem

--- src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistory.kt
+++ src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistory.kt
@@ -14,6 +14,10 @@ class UserLoanHistory(
     val id: Long? = null,
 ) {
 
+    val isReturn: Boolean
+        get() = this.status == UserLoanStatus.RETURNED
+
+
     fun doReturn() {
         this.status = UserLoanStatus.RETURNED
     }
